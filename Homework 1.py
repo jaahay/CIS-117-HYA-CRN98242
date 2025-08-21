c43f82c96faa3e52e47667723dec514ec4a633c0
jaahay <jaahay+github@gmail.com>
2025-08-20 23:02:30 -0700

    Homework 1

---
+++ Homework 1.py
@@ -0,0 +1,21 @@
+# Favorite Song
+print("Hello! What is your name?")
+name = input()
+print("Nice to meet you, " + name + ".")
+
+print("What is your favorite song?")
+song = input()
+print("Cool! I like", song, "too.")
+
+# Frost poem
+print("I shall be telling this with a sigh")
+print("Somewhere ages and ages hence:")
+print("Two roads diverged in a wood, and I--")
+print("I took the one less traveled by,")
+print("And that has made all the difference.")
+
+# Name and likes
+name = input("What is your name? ")
+likes = input("What do you like? ")
+
+print(name + " likes " + likes)
