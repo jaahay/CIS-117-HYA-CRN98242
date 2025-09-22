5748dc58d8cb4dcc6aa9154f07a129358492587c
jaahay <jaahay@users.noreply.github.com>
2025-09-21 21:14:39 -0700

    awkward typing but ehh

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -11,35 +11,49 @@ the program will decide on who won or if it is a tie.
 
 import random
 
+rock = "Rock"
+paper = "Paper"
+scissors = "Scissors"
+
+rock_paper_scissors = {
+    1: rock, rock: 1,
+    2: paper, paper: 2,
+    3: scissors, scissors: 3
+}
+
 def rockpaperscissors_game():
     Computer_Selection = random.randint(1, 3)
+    Computer_Throw = rock_paper_scissors[Computer_Selection]
+
     User_Selection = int(input("Select your throw:\n" \
         "1. Rock\n" \
         "2. Paper\n" \
         "3. Scissors\n"
     ))
-    print(f"You're throwing {throw_text(User_Selection)}")
+    User_Throw = rock_paper_scissors[User_Selection]
+
+    print("You're throwing", User_Throw)
     print("Let's see what the computer will try..")
-    print(f"The computer threw {throw_text(Computer_Selection)}")
+    print("The computer threw", Computer_Throw)
     
-    print(score_throw(User_Selection, Computer_Selection))
-    
-def throw_text(i):
-    if(i == 1): return "Rock"
-    if(i == 2): return "Paper"
-    if(i == 3): return "Scissors"
+    print(score_throw(User_Throw, Computer_Throw))
 
 def score_throw(My_Throw, Their_Throw):
     if(My_Throw == Their_Throw): return "It's a tie!"
-    elif(My_Throw == 1 and Their_Throw == 2
-        or My_Throw == 2 and Their_Throw == 3
-        or My_Throw == 3 and Their_Throw == 1):
+    elif(
+        My_Throw == rock and Their_Throw == paper
+        or My_Throw == paper and Their_Throw == scissors
+        or My_Throw == scissors and Their_Throw == rock
+        ):
         return "Sorry, you lose."
-    elif(My_Throw == 1 and Their_Throw == 3
-         or My_Throw == 2 and Their_Throw == 1
-         or My_Throw == 3 and Their_Throw == 2):
+    elif(
+        My_Throw == rock and Their_Throw == scissors
+        or My_Throw == paper and Their_Throw == rock
+        or My_Throw == scissors and Their_Throw == paper
+        ):
         return "Congratulations, you win!"
     else:
         return "I didn't understand that."
     
-    
+if __name__ == "__main__":
+    rockpaperscissors_game()
