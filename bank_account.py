0843bef9d95ac81a0326ecd6899cd8a81dc8800a
jaahay <jaahay@users.noreply.github.com>
2025-10-23 13:40:51 -0700

    bank account example __main__

--- bank_account.py
+++ bank_account.py
@@ -1,3 +1,6 @@
+from savings_account import *
+from checking_account import *
+
 class BankAccount:
     'class that implements the functionality of a bank account'
     
@@ -29,3 +32,9 @@ class BankAccount:
     def generate_account_number(cls):
         BankAccount.key += 1
         return BankAccount.key
+    
+if __name__ == "__main__":
+    anna_account = SavingsAccount(1000)
+    joe_account = SavingsAccount(2000, 0.2)
+    anna_account.deposit(500)
+    joe_account.deposit(100)
