8705e691f43508eea23f5bc52b6cc15fc944a407
jaahay <jaahay+github@gmail.com>
2025-11-13 20:47:22 -0800

    wow! email regex

--- homework14/Homework14_hay_james.py
+++ homework14/Homework14_hay_james.py
@@ -1,8 +1,15 @@
 from html.parser import HTMLParser
-from collections import Counter
-import string
 import re
-import sys
+
+def make_wow_email_regex():
+    '''
+    see: https://stackoverflow.com/questions/201323/how-can-i-validate-an-email-address-using-a-regular-expression
+    I modified it to prevent things like "?cc=" and "cc=". They will still be captured.
+    '''
+    # return r"(?:[a-z0-9!#$%&'*+\x2f=?^_`\x7b-\x7d~\x2d]+(?:\.[a-z0-9!#$%&'*+\x2f=?^_`\x7b-\x7d~\x2d]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9\x2d]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9\x2d]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9\x2d]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+    return r"(?:[a-z0-9\x2f\x7b-\x7d~\x2d]+(?:\.[a-z0-9!#$%&'*+\x2f=?^_`\x7b-\x7d~\x2d]+)*|\"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9\x2d]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9\x2d]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9\x2d]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"
+
+wow_email_regex = make_wow_email_regex()
 
 class MyHTMLParser(HTMLParser):
     
@@ -13,32 +20,35 @@ class MyHTMLParser(HTMLParser):
     """
     def __init__(self):
         super().__init__()
-        email_regex = r"^[a_zA_Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        self.emails = []
 
     def handle_starttag(self, tag, attrs):
-        if "a" == tag.lower():
+        if "a" != tag.lower():
             return
-        starttag_search = re.search(self.email_regex, attrs.join())
+        href = [item for item in attrs if item[0] == "href"]
+        href = href[0]
+        if not href[1].startswith("mailto:"):
+            return
+        self.emails.append(href[1])
 
-        pass
-        
     def handle_data(self, data):
-        pass
+        # email_groups = re.finditer(self.email_regex, data)
+        email_groups = re.finditer(wow_email_regex, data)
+        # if email_groups is None:
+        #     return
+        for email in email_groups:
+            self.emails.append(email.group(0))
 
 if __name__ == '__main__':
     # Fetch and parse the page again
-    link = 'https://collegeofsanmateo.edu/wellnesscenter'
+    # link = 'https://collegeofsanmateo.edu/wellnesscenter'
+    link = 'https://maildiver.com/blog/mailto-links-complete-guide/'
     from urllib.request import urlopen
 
     resp = urlopen(link)
     html = resp.read().decode().lower()
 
-    main = main_search.group()
-    # print(main)
     parser = MyHTMLParser()
-    parser.feed(main)
+    parser.feed(html)
 
-    # Show frequency for words occurring >= 10 times
-    parser.frequency(6)
-    print(parser.counter.get("wellness"))
-    parser.print_clean_data()
+    print(parser.emails)
